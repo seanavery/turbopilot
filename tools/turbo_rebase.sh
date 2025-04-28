0bcadda97fe5ea81e0ed15514971471b1922bc7d
seanavery <seanpollock3344@gmail.com>
2025-04-27 23:41:32 -0400

    More script

--- tools/turbo_rebase.sh
+++ tools/turbo_rebase.sh
@@ -4,6 +4,7 @@ set -e
 if [ -d .git/rebase-merge ] || [ -d .git/rebase-apply ]; then
   echo "Aborting ongoing rebase..."
   git rebase --abort
+  git merge --abort
 fi
 
 echo "Fetching updates from origin (openpilot) and turbo (your fork)..."
@@ -24,8 +25,29 @@ echo "Returning to turbo branch..."
 git checkout turbo
 
 # # Create merge on opendbc submodule turbo/turbo brannch with the commit hash from origin/master
-# echo "Creating merge commit on opendbc submodule turbo branch with commit hash ${OPENPILOT_OPENDBC_COMMIT}..."
-# cd opendbc_repo
+echo "Creating merge commit on opendbc submodule turbo branch with commit hash ${OPENPILOT_OPENDBC_COMMIT}..."
+cd opendbc_repo
+
+# check if in the middle of a rebase or merge and exit if so
+if [ -d .git/rebase-merge ] || [ -d .git/rebase-apply ]; then
+  echo "Aborting ongoing rebase..."
+  git rebase --abort
+  git merge --abort
+fi
+git fetch origin master
+
+# check if the commit hash is already in the opendbc_repo turbo branch
+if git rev-parse --verify --quiet "turbo/${OPENPILOT_OPENDBC_COMMIT}"; then
+  echo "The commit ${OPENPILOT_OPENDBC_COMMIT} is already in the opendbc_repo turbo branch. Exiting..."
+  exit 0
+fi
+
+# Create a merge commit on the turbo branch
+git merge --no-ff -m "Merge commit ${OPENPILOT_OPENDBC_COMMIT} from origin/master into turbo branch" "${OPENPILOT_OPENDBC_COMMIT}" || {
+  echo "Merge conflict detected. Aborting merge..."
+  git merge --abort
+  exit 1
+}
 
 # # Stage the updated submodule pointer
 # git add opendbc_repo
